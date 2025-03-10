2ecbf6841fc7d6bc57cf0d26b142ec02edb8d078
webdevkaleem <webdevkaleem@gmail.com>
2025-03-10 19:26:47 +0500

    Minor Changes Button Uploadthing: Removed not used store constants and functions

--- store/button-uploadthing-store.ts
+++ store/button-uploadthing-store.ts
@@ -16,23 +16,15 @@ export interface UploadFile {
 }
 
 interface FilesState {
-  files: UploadFile[];
   historicFiles: UploadFile[];
-  displayModel: boolean;
-  addFiles: (newFiles: UploadFile[]) => void;
   setFiles: (newFiles: UploadFile[]) => void;
   updateFileStatus: (id: string, status: FileStatus, url?: string) => void;
   removeFile: (id: string) => void;
-  resetFiles: () => void;
-  openModel: () => void;
-  closeModel: () => void;
-  toggleModel: () => void;
 }
 
 export const useFilesStore = create<FilesState>((set) => ({
   files: [],
   historicFiles: [],
-  displayModel: false,
   setFiles: (newFiles) =>
     set((state) => {
       // Check if the file is already in the historicFiles array. If not, add it as well
@@ -48,40 +40,14 @@ export const useFilesStore = create<FilesState>((set) => ({
         historicFiles: [...state.historicFiles, ...newHistoricFiles],
       };
     }),
-  addFiles: (newFiles) =>
-    set((state) => {
-      // Check if the file is already in the historicFiles array. If not, add it as well
-      const newHistoricFiles = Array.from(newFiles).filter(
-        (file) =>
-          !state.historicFiles.some(
-            (historicFile) => historicFile.id === file.id
-          )
-      );
-
-      return {
-        files: [...state.files, ...newFiles],
-        historicFiles: [...state.historicFiles, ...newHistoricFiles],
-      };
-    }),
   updateFileStatus: (id, status, url) =>
     set((state) => ({
-      files: state.files.map((item) =>
-        item.id === id ? { ...item, status, url } : item
-      ),
       historicFiles: state.historicFiles.map((item) =>
         item.id === id ? { ...item, status, url } : item
       ),
     })),
   removeFile: (id) =>
     set((state) => ({
-      files: state.files.filter((item) => item.id !== id),
       historicFiles: state.historicFiles.filter((item) => item.id !== id),
     })),
-  resetFiles: () =>
-    set({
-      files: [],
-    }),
-  openModel: () => set({ displayModel: true }),
-  closeModel: () => set({ displayModel: false }),
-  toggleModel: () => set((state) => ({ displayModel: !state.displayModel })),
 }));
